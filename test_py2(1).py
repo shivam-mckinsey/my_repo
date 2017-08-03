b440f67aeac086e19655aefe82f31d4b60ce9881
Shivam Kotwalia <shivamkotwalia@gmail.com>
2017-08-04 00:27:41 +0530

    updated hostname & port parameter

--- test_py2(1).py
+++ test_py2(1).py
@@ -9,16 +9,18 @@ db_username = 'odoo'
 db_password = 'odoo'
 db_names = ['demo_blitzzcar']
 curr_month = int(time.strftime('%Y%m%d'))
+host = 'localhost'
+port = '5432'
 
 for db_name in db_names:
     try:
         file_path = ''
-        dumper = " -U %s -Z 9 -f %s -F c %s  "
+        dumper = " -U %s -Z 9 -f %s -F c %s -h %s -p %s"
         os.putenv('redhat', db_password)
         bkp_file = '%s_%s.sql' % (db_name, time.strftime('%Y%m%d_%H_%M_%S'))
 #        glob_list = glob.glob(dump_dir + db_name + '*' + '.pgdump')
         file_path = os.path.join(dump_dir, bkp_file)
-        command = 'pg_dump' + dumper % (db_username, file_path, db_name)
+        command = 'pg_dump' + dumper % (db_username, file_path, db_name, hostname, port)
         subprocess.call(command, shell=True)
         subprocess.call('gzip ' + file_path, shell=True)
     except:
